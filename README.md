e07e1ae891541aab5ba3629206e04c64b6a9aacb
Hampus <ht222mi@student.lnu.se>
2024-10-20 19:16:04 +0200

    Inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hampus-portfolio
+
+My own portfolio website that I have created using Vue and Tailwind CSS
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: "Welcome to Hampus Portfolio",
+        link: "https://github.com/HampusTuisku"
+      }
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <a v-bind:href="link">GitHub</a>
+</template>
+
+<style>
+</style>
+
